923e6bf344fce42b3707ace5703e8f4b8f3abf00
JosephAzizli <yusif.azizli.04@gmail.com>
2022-11-18 02:16:04 +0400

    Last changes

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-let number1 = document.getElementById("n1")
-let number2 = document.getElementById("n2")
-let calcBtn = document.getElementById("btn")
-let result = document.getElementById("result")
+let number1 = document.getElementById("n1");
+let number2 = document.getElementById("n2");
+let calcBtn = document.getElementById("btn");
+let result = document.getElementById("result");
 
-calcBtn.onclick = function(){
-    if(Number(number1.value)%2 == 0 & Number(number2.value)%2 == 0){
-        let sum = Math.pow( Number(number1.value) ,2) +  Math.pow(Number(number2.value) ,2)
-        result.innerHTML = sum
-    }
-  else{
-    alert("Please enter even numbers :)")
-}
-} 
+calcBtn.onclick = function () {
+  if ((Number(number1.value) % 2 == 0) & (Number(number2.value) % 2 == 0)) {
+    let sum =
+      Math.pow(Number(number1.value), 2) + Math.pow(Number(number2.value), 2);
+    result.innerHTML = sum;
+  } else {
+    alert("Please enter even numbers :)");
+  }
+};
